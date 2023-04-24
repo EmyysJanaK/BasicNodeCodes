2ee63ebd2d80b9859b9a2235cf35175314641a41
JanakCSE210598B <129370684+JanakCSE210598B@users.noreply.github.com>
2023-04-25 03:43:02 +0530

    Create createserver.js

---
+++ createserver.js
@@ -0,0 +1,12 @@
+var http = require('http'); //Inlcude built-in moddule to transfer data over the HTTP.
+
+// Create HTTP server object.
+http.createServer(
+    function(req,res){
+        /* Include the suitable(html) header to be dispalyed the server response in html*/
+        res.writeHead(200,{'Content-Type': 'text/html'}); 
+        res.write("This is an private network!."); // Write the response to the client.
+        res.end(); // End the response.
+        
+    }
+).listen(8080); // Port 8080 is listened by HTTP server object.
